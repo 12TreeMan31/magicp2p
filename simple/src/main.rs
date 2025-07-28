f13ed86563e0b90e44f26bf7bbbb567790da7e4b
David Coulson <theinvisiblebench2@gmail.com>
2025-07-28 16:21:51 -0500

    Simple: now waits to be connect to DHT and some refactoring

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -1,21 +1,20 @@
 use futures::prelude::*;
 use libp2p::{
-    Multiaddr, SwarmBuilder,
-    gossipsub::Sha256Topic,
-    identify,
-    kad::{self, store::MemoryStore},
+    SwarmBuilder, identify,
+    kad::{self, QueryId, QueryResult, store::MemoryStore},
+    multiaddr::{Multiaddr, Protocol},
     noise,
     swarm::{NetworkBehaviour, SwarmEvent},
     tcp, yamux,
 };
-use libp2p_identity::{Keypair, PeerId};
+use libp2p_identity::Keypair;
 use std::error::Error;
 
 const IPFS_BOOTNODES: [&str; 4] = [
-    "QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-    "QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-    "QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-    "QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
 ];
 
 #[derive(NetworkBehaviour)]
@@ -24,10 +23,48 @@ struct Behaviour {
     identify: identify::Behaviour,
 }
 
+impl Behaviour {
+    pub fn new(keys: &Keypair) -> (Self, QueryId) {
+        let bootstrap_querry_id;
+        let peer_id = keys.public().to_peer_id();
+
+        let kademlia = {
+            let cfg = kad::Config::new(kad::PROTOCOL_NAME);
+            let mut kademlia = kad::Behaviour::with_config(peer_id, MemoryStore::new(peer_id), cfg);
+
+            for addr in IPFS_BOOTNODES {
+                let addr: Multiaddr = addr.parse().expect("Valid multiaddr");
+                let peer_id = match addr.clone().pop().unwrap() {
+                    Protocol::P2p(peer_id) => peer_id,
+                    _ => unimplemented!("Handle this in the real app"),
+                };
+
+                kademlia.add_address(&peer_id, addr);
+            }
+            bootstrap_querry_id = kademlia.bootstrap().expect("Could not bootstrap");
+
+            kademlia
+        };
+
+        let identify = {
+            let cfg = identify::Config::new(identify::PROTOCOL_NAME.to_string(), keys.public());
+            let identify = identify::Behaviour::new(cfg);
+
+            identify
+        };
+
+        (Self { kademlia, identify }, bootstrap_querry_id)
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let key = Keypair::generate_ed25519();
-    let mut swarm = SwarmBuilder::with_existing_identity(key.clone())
+    let keys = Keypair::generate_ed25519();
+    println!("Local PeerID {}", keys.public().to_peer_id());
+
+    let mut bootstrap_querry_id = None;
+
+    let mut swarm = SwarmBuilder::with_existing_identity(keys.clone())
         .with_tokio()
         .with_tcp(
             tcp::Config::default(),
@@ -36,42 +73,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
         )?
         .with_dns()?
         .with_behaviour(|keys| {
-            let kademlia_cfg = kad::Config::new(kad::PROTOCOL_NAME);
-            let peer_id = keys.public().to_peer_id();
-            let mut kademlia =
-                kad::Behaviour::with_config(peer_id, MemoryStore::new(peer_id), kademlia_cfg);
+            let (behaviour, id) = Behaviour::new(keys);
+            bootstrap_querry_id = Some(id);
 
-            let bootaddr: Multiaddr = "/dnsaddr/bootstrap.libp2p.io"
-                .parse()
-                .expect("Valid multiaddr");
-
-            for peer_id in IPFS_BOOTNODES {
-                let peer_id: PeerId = peer_id.parse().expect("Valid PeerId");
-                kademlia.add_address(&peer_id, bootaddr.clone());
-            }
-            let _ = kademlia.bootstrap();
-
-            let identify_cfg =
-                identify::Config::new(identify::PROTOCOL_NAME.to_string(), keys.public());
-            let identify = identify::Behaviour::new(identify_cfg);
-
-            println!("{:?}", peer_id);
-
-            Behaviour { kademlia, identify }
+            behaviour
         })?
         .build();
     swarm.listen_on("/ip6/::/tcp/0".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    let key = Sha256Topic::new("hello-dave").hash();
-    println!("{}", key);
-    let sub_id = swarm
-        .behaviour_mut()
-        .kademlia
-        .start_providing(key.clone().into_string().into_bytes().into())
-        .expect("Could not put key");
+    let mut get_providers_querry_id = None;
+    let mut providing_querry_id = None;
 
-    let mut id2 = sub_id;
+    let key: Vec<u8> = b"hello-dave".to_vec();
+
     loop {
         let event = swarm.select_next_some().await;
         match event {
@@ -86,15 +101,48 @@ async fn main() -> Result<(), Box<dyn Error>> {
             },
             SwarmEvent::Behaviour(BehaviourEvent::Kademlia(x)) => match x {
                 kad::Event::OutboundQueryProgressed { result, id, .. } => {
-                    if sub_id == id {
-                        println!("{:?}", result);
-                        id2 = swarm
+                    if Some(id) == bootstrap_querry_id {
+                        if let QueryResult::Bootstrap(x) = result {
+                            let ok = x.expect("Error bootstrapping");
+                            print!("{}, ", ok.num_remaining);
+                            if ok.num_remaining == 0 {
+                                println!("\nConnected to the DHT!");
+
+                                let id = swarm
+                                    .behaviour_mut()
+                                    .kademlia
+                                    .start_providing(key.clone().into())?;
+                                providing_querry_id = Some(id);
+                                bootstrap_querry_id = None;
+                            }
+                        }
+                    } else if Some(id) == providing_querry_id {
+                        if let QueryResult::StartProviding(x) = result {
+                            let ok = x.expect("Could not provide key");
+                            println!("Started providing key: {:?}", ok.key);
+                        }
+
+                        let id = swarm
                             .behaviour_mut()
                             .kademlia
-                            .get_providers(key.clone().into_string().into_bytes().into());
-                    }
-                    if id2 == id {
-                        println!("{:?}", result);
+                            .get_providers(key.clone().into());
+
+                        get_providers_querry_id = Some(id);
+                        providing_querry_id = None;
+                    } else if Some(id) == get_providers_querry_id {
+                        if let QueryResult::GetProviders(x) = result {
+                            let ok = x.expect("Could not get providers");
+                            match ok {
+                                kad::GetProvidersOk::FoundProviders { providers, .. } => {
+                                    for provider in providers {
+                                        println!("{}", provider);
+                                    }
+                                }
+                                kad::GetProvidersOk::FinishedWithNoAdditionalRecord { .. } => {
+                                    get_providers_querry_id = None;
+                                }
+                            }
+                        }
                     }
                 }
                 _ => {}
