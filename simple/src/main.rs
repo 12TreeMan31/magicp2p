f92fc4ef3c812af1f15239c82e16cb8a5e71daac
David Coulson <theinvisiblebench2@gmail.com>
2025-08-28 22:23:02 -0500

    Simple: Removed everything and made it truly simple

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -1,45 +1,21 @@
 // https://github.com/ipfs/kubo/blob/master/docs/config.md#addresses
 
-use clap::Parser;
 use futures::prelude::*;
 use libp2p::{
-    SwarmBuilder, autonat, dcutr, identify,
-    kad::{self, QueryId, QueryResult, store::MemoryStore},
-    multiaddr::{Multiaddr, Protocol},
-    noise, relay,
+    SwarmBuilder,
+    gossipsub::{self, MessageAuthenticity},
+    mdns, noise,
     swarm::{NetworkBehaviour, Swarm, SwarmEvent},
     tcp, yamux,
 };
-use libp2p_identity::{Keypair, PeerId};
+use libp2p_identity::Keypair;
 use std::error::Error;
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    // If passed will start in server mode rather than client mode
-    #[arg(short, long)]
-    server: bool,
-
-    // By defult bootstraps to main ipfs nodes, or use multiaddr for bootstrap
-    #[arg(short, long)]
-    bootnode: Option<Multiaddr>,
-}
-
-const IPFS_BOOTNODES: [&str; 4] = [
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
-];
+use tokio::{self, io, io::AsyncBufReadExt, select};
 
 #[derive(NetworkBehaviour)]
 struct Behaviour {
-    kademlia: kad::Behaviour<MemoryStore>,
-    identify: identify::Behaviour,
-    relay: relay::Behaviour,
-    dcutr: dcutr::Behaviour,
-    autonat: autonat::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+    gossipsub: gossipsub::Behaviour,
 }
 
 impl Behaviour {
@@ -47,135 +23,38 @@ impl Behaviour {
     pub fn new(keys: &Keypair) -> Self {
         let peer_id = keys.public().to_peer_id();
 
-        let kademlia_cfg = kad::Config::new(kad::PROTOCOL_NAME);
-        let mut kademlia =
-            kad::Behaviour::with_config(peer_id, MemoryStore::new(peer_id), kademlia_cfg);
-        // We need the peerid but also need the peerid in the multiaddr because reasons.
-        // While it may be overkill, in the real program this will allow us to add custom bootnodes
-        // from just the multiaddr
-        for addr in IPFS_BOOTNODES {
-            let addr: Multiaddr = addr.parse().expect("Valid multiaddr");
-            let peer_id = match addr.clone().pop().unwrap() {
-                Protocol::P2p(peer_id) => peer_id,
-                _ => unimplemented!("Handle this in the real app"),
-            };
+        let mdns = mdns::tokio::Behaviour::new(mdns::Config::default(), peer_id).unwrap();
 
-            kademlia.add_address(&peer_id, addr);
-        }
-        println!("Mode: {}", kademlia.mode());
+        // Set a custom gossipsub configuration
+        let gossipsub_cfg = gossipsub::ConfigBuilder::default().build().unwrap();
+        let gossipsub =
+            gossipsub::Behaviour::new(MessageAuthenticity::Signed(keys.clone()), gossipsub_cfg)
+                .unwrap();
 
-        let identify_cfg =
-            identify::Config::new(identify::PROTOCOL_NAME.to_string(), keys.public())
-                .with_push_listen_addr_updates(true);
-        let identify = identify::Behaviour::new(identify_cfg);
-
-        let realy_cfg = relay::Config::default();
-        let relay = relay::Behaviour::new(peer_id, realy_cfg);
-
-        let dcutr = dcutr::Behaviour::new(peer_id);
-
-        let autonat = autonat::Behaviour::new(peer_id, Default::default());
-
-        Self {
-            kademlia,
-            identify,
-            relay,
-            dcutr,
-            autonat,
-        }
-    }
-    /// For now just adds to the DHT but later make sure to also check autonat
-    pub fn add_peer(&mut self, peer_id: &PeerId, addrs: &[Multiaddr]) {
-        for addr in addrs {
-            self.kademlia.add_address(peer_id, addr.clone());
-        }
+        Self { mdns, gossipsub }
     }
 }
 
-enum KadState {
-    Bootstrap,
-    Providing,
-    Listening,
-}
-struct ProviderState {
-    step: KadState,
-    id: QueryId,
-}
-
-fn new_peer(swarm: &mut Swarm<Behaviour>, peer_id: &PeerId, info: identify::Info) {
-    // We are only allowed to connect to servers per the kad spec
-    if !info.protocols.contains(&kad::PROTOCOL_NAME) || swarm.is_connected(peer_id) {
-        return;
-    }
-
-    // This could be a bug, the spec doesn't require observedAddr: https://github.com/libp2p/specs/blob/master/identify/README.md#the-identify-message
-    swarm.add_external_address(info.observed_addr.clone());
-
-    swarm.dial(info.listen_addrs[0].clone()).unwrap();
-    swarm.behaviour_mut().add_peer(peer_id, &info.listen_addrs);
-}
-
-async fn handle_kad(
-    kad: &mut kad::Behaviour<MemoryStore>,
-    state: &mut ProviderState,
-    event: kad::Event,
-    key: kad::RecordKey,
-) {
+fn network_event(swarm: &mut Swarm<Behaviour>, event: SwarmEvent<BehaviourEvent>) {
     match event {
-        kad::Event::ModeChanged { new_mode } => println!("Mode has been updated to {}", new_mode),
-        kad::Event::OutboundQueryProgressed { id, result, .. } => {
-            if id != state.id {
-                return;
+        SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+            for (id, addr) in list {
+                println!("New Peer {addr}");
+                swarm.behaviour_mut().gossipsub.add_explicit_peer(&id);
             }
-
-            match state.step {
-                KadState::Bootstrap => {
-                    let ok = match result {
-                        QueryResult::Bootstrap(Ok(x)) => x,
-                        _ => panic!("Failed to bootstrap {:?}", result),
-                    };
-
-                    print!("{}, ", ok.num_remaining);
-                    if ok.num_remaining != 0 {
-                        return;
-                    }
-
-                    println!("Connected to the DHT!");
-                    let id = kad.start_providing(key.clone()).unwrap();
-
-                    state.step = KadState::Providing;
-                    state.id = id
-                }
-                KadState::Providing => {
-                    let ok = match result {
-                        QueryResult::StartProviding(Ok(x)) => x,
-                        _ => panic!("Could not provide key: {:?}", result),
-                    };
-                    println!("Started providing key: {:?}", ok.key);
-
-                    // tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-
-                    let id = kad.get_providers(ok.key);
-
-                    state.step = KadState::Listening;
-                    state.id = id;
-                }
-                KadState::Listening => {
-                    let ok = match result {
-                        QueryResult::GetProviders(Ok(x)) => x,
-                        _ => panic!("Couldn't get providers: {:?}", result),
-                    };
-
-                    match ok {
-                        kad::GetProvidersOk::FoundProviders { providers, .. } => {
-                            for provider in providers {
-                                println!("{}", provider);
-                            }
-                        }
-                        kad::GetProvidersOk::FinishedWithNoAdditionalRecord { .. } => {}
-                    }
-                }
+        }
+        SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+            for (id, addr) in list {
+                println!("Remove Peer {addr}");
+                swarm.behaviour_mut().gossipsub.remove_blacklisted_peer(&id);
             }
+        }
+        SwarmEvent::Behaviour(BehaviourEvent::Gossipsub(gossipsub::Event::Message {
+            propagation_source: peer_id,
+            message,
+            ..
+        })) => {
+            println!("<{peer_id}>: {}", String::from_utf8_lossy(&message.data),)
         }
         _ => {}
     }
@@ -183,8 +62,6 @@ async fn handle_kad(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-
     let keys = Keypair::generate_ed25519();
     println!("Local PeerID {}", keys.public().to_peer_id());
 
@@ -201,68 +78,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     swarm.listen_on("/ip6/::/tcp/0".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    // swarm.listen_on("/ip6/::/udp/0/quic-v1".parse()?)?;
-    // swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
-    let bootstrap_querry_id = swarm
-        .behaviour_mut()
-        .kademlia
-        .bootstrap()
-        .expect("Could not bootstrap");
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
 
-    // let mut get_providers_querry_id = None;
-    // let mut providing_querry_id = None;
-
-    let key: Vec<u8> = b"hello-dave".to_vec();
-    let mut state: ProviderState = ProviderState {
-        step: KadState::Bootstrap,
-        id: bootstrap_querry_id,
-    };
+    let topic = gossipsub::IdentTopic::new("hi-dave");
+    swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
     loop {
-        let total_peers = swarm.connected_peers().count();
-        if total_peers != 0 {
-            // println!("Connected Peers: {}", total_peers);
-        }
-        let event = swarm.select_next_some().await;
-        // println!("{:?}", event);
-        match event {
-            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {}", address),
-            SwarmEvent::Behaviour(BehaviourEvent::Identify(x)) => match x {
-                identify::Event::Received { peer_id, info, .. } => {
-                    new_peer(&mut swarm, &peer_id, info);
+        select! {
+            Ok(Some(line)) = stdin.next_line() => {
+                if let Err(e) = swarm
+                    .behaviour_mut().gossipsub
+                    .publish(topic.clone(), line.as_bytes()) {
+                    println!("Publish error: {e:?}");
                 }
-                _ => {}
-            },
-            SwarmEvent::Behaviour(BehaviourEvent::Kademlia(x)) => {
-                handle_kad(
-                    &mut swarm.behaviour_mut().kademlia,
-                    &mut state,
-                    x,
-                    key.clone().into(),
-                )
-                .await;
             }
-            SwarmEvent::ExternalAddrConfirmed { address } => {
-                println!(
-                    "Mode updated to {} at {}",
-                    swarm.behaviour_mut().kademlia.mode(),
-                    address
-                )
-            }
-            SwarmEvent::ExternalAddrExpired { address } => {
-                println!(
-                    "Could not confirm external address: {}, switching to server mode",
-                    address
-                );
-                swarm
-                    .behaviour_mut()
-                    .kademlia
-                    .set_mode(Some(kad::Mode::Server));
-            }
-            SwarmEvent::OutgoingConnectionError { error, .. } => {
-                println!("Error dialing: {}", error)
-            }
-            _ => {}
+            event = swarm.select_next_some() => network_event(&mut swarm, event)
         }
     }
 }
