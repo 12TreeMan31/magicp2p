06d87a56e9c10616d748c78f9c0efa73639fb734
David Coulson <theinvisiblebench2@gmail.com>
2025-08-28 20:38:25 -0500

    Simple: Added handling for more events

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # magicp2p
+
+## Steps in order to connect to Amino
+Make sure you have support to RSA and ED25519 in libp2p-identity
+RSA is the old standard that is being [fazed out](link).
+Once you have that create a ED25519 keypair to give to the swarm.
+
+#### Intialize the network behavour with these protocals:
+- kademlia      (the dht)
+- identify      (for sharing supported protocals)
+- autonat-v2*    (for what ip addrs are reachable and can be shared on animo)
+- relay         (for ipv4)
+- dcutr     (unkown if needed)
+
+*Autonat-v1 is being deprecated by [kubo](https://github.com/ipfs/kubo/releases#kubo-now-uses-autonatv2-as-a-client); despite this, rust-libp2ps [default behavour](https://docs.rs/libp2p/latest/libp2p/autonat/struct.Behaviour.html) is still using v1 and so you must explicitly use v2.
+
+- Create the swarm with the ED25519 key and wanted transports (which should just be dns, tcp, and quic-v1*)
+- Listen on ipv6 and ipv4
+
+*Do not support quic [draft 29](https://github.com/libp2p/specs/blob/master/quic/README.md#quic-versions)
+
+#### Next we should be ready to bootstrap to the ipfs bootnodes:
+
+```mermaid
+graph LR;
+    A[Starting]
+    B[Discovery]
+
+    A--bootstrap-->B 
+```
+
+```
+/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN
+/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa
+/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb
+/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt
+```
+
+We add these to the DHT using kademlias `add_address()` and its important to know that you need to have the [peerID in the multiaddr](rust-libp2p github issue).
+
+Now you can call `bootstrap()` and you *should* be connected to Amino as a [client](link that explains what a client can do)
+
+While you might be connected to Animo, being a client isn't that useful and limits you a lot in what you can do. In order to use the network to its fullest we need to become a [server](link that talks about servers).
+
+In order to become a server there are a couple [requirments](link) we must meet.
+
+See
+https://blog.ipfs.tech/2020-07-20-dht-deep-dive/
+https://mermaid.js.org/
--- simple/src/main.rs
+++ simple/src/main.rs
@@ -1,27 +1,45 @@
+// https://github.com/ipfs/kubo/blob/master/docs/config.md#addresses
+
+use clap::Parser;
 use futures::prelude::*;
 use libp2p::{
-    SwarmBuilder, identify,
+    SwarmBuilder, autonat, dcutr, identify,
     kad::{self, QueryId, QueryResult, store::MemoryStore},
     multiaddr::{Multiaddr, Protocol},
-    noise,
-    swarm::{NetworkBehaviour, SwarmEvent},
+    noise, relay,
+    swarm::{NetworkBehaviour, Swarm, SwarmEvent},
     tcp, yamux,
 };
-use libp2p_identity::Keypair;
+use libp2p_identity::{Keypair, PeerId};
 use std::error::Error;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // If passed will start in server mode rather than client mode
+    #[arg(short, long)]
+    server: bool,
+
+    // By defult bootstraps to main ipfs nodes, or use multiaddr for bootstrap
+    #[arg(short, long)]
+    bootnode: Option<Multiaddr>,
+}
+
 const IPFS_BOOTNODES: [&str; 4] = [
-    "/dns6/bootstrap.libp2p.io/tcp/4001/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-    "/dns6/bootstrap.libp2p.io/tcp/4001/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-    "/dns6/bootstrap.libp2p.io/tcp/4001/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-    "/dns6/bootstrap.libp2p.io/tcp/4001/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
 ];
 
 #[derive(NetworkBehaviour)]
 struct Behaviour {
     kademlia: kad::Behaviour<MemoryStore>,
     identify: identify::Behaviour,
-    // autonat: autonat::Behaviour,
+    relay: relay::Behaviour,
+    dcutr: dcutr::Behaviour,
+    autonat: autonat::Behaviour,
 }
 
 impl Behaviour {
@@ -47,10 +65,30 @@ impl Behaviour {
         println!("Mode: {}", kademlia.mode());
 
         let identify_cfg =
-            identify::Config::new(identify::PROTOCOL_NAME.to_string(), keys.public());
+            identify::Config::new(identify::PROTOCOL_NAME.to_string(), keys.public())
+                .with_push_listen_addr_updates(true);
         let identify = identify::Behaviour::new(identify_cfg);
 
-        Self { kademlia, identify }
+        let realy_cfg = relay::Config::default();
+        let relay = relay::Behaviour::new(peer_id, realy_cfg);
+
+        let dcutr = dcutr::Behaviour::new(peer_id);
+
+        let autonat = autonat::Behaviour::new(peer_id, Default::default());
+
+        Self {
+            kademlia,
+            identify,
+            relay,
+            dcutr,
+            autonat,
+        }
+    }
+    /// For now just adds to the DHT but later make sure to also check autonat
+    pub fn add_peer(&mut self, peer_id: &PeerId, addrs: &[Multiaddr]) {
+        for addr in addrs {
+            self.kademlia.add_address(peer_id, addr.clone());
+        }
     }
 }
 
@@ -62,6 +100,19 @@ enum KadState {
 struct ProviderState {
     step: KadState,
     id: QueryId,
+}
+
+fn new_peer(swarm: &mut Swarm<Behaviour>, peer_id: &PeerId, info: identify::Info) {
+    // We are only allowed to connect to servers per the kad spec
+    if !info.protocols.contains(&kad::PROTOCOL_NAME) || swarm.is_connected(peer_id) {
+        return;
+    }
+
+    // This could be a bug, the spec doesn't require observedAddr: https://github.com/libp2p/specs/blob/master/identify/README.md#the-identify-message
+    swarm.add_external_address(info.observed_addr.clone());
+
+    swarm.dial(info.listen_addrs[0].clone()).unwrap();
+    swarm.behaviour_mut().add_peer(peer_id, &info.listen_addrs);
 }
 
 async fn handle_kad(
@@ -102,7 +153,7 @@ async fn handle_kad(
                     };
                     println!("Started providing key: {:?}", ok.key);
 
-                    tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+                    // tokio::time::sleep(std::time::Duration::from_secs(60)).await;
 
                     let id = kad.get_providers(ok.key);
 
@@ -132,6 +183,8 @@ async fn handle_kad(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
     let keys = Keypair::generate_ed25519();
     println!("Local PeerID {}", keys.public().to_peer_id());
 
@@ -142,15 +195,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
             noise::Config::new,
             yamux::Config::default,
         )?
-        .with_quic()
         .with_dns()?
         .with_behaviour(|keys| Behaviour::new(keys))?
         .build();
     swarm.listen_on("/ip6/::/tcp/0".parse()?)?;
-    //     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    swarm.listen_on("/ip6/::/udp/0/quic-v1".parse()?)?;
-    //  swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
+    // swarm.listen_on("/ip6/::/udp/0/quic-v1".parse()?)?;
+    // swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
     let bootstrap_querry_id = swarm
         .behaviour_mut()
         .kademlia
@@ -169,7 +221,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         let total_peers = swarm.connected_peers().count();
         if total_peers != 0 {
-            println!("Connected Peers: {}", total_peers);
+            // println!("Connected Peers: {}", total_peers);
         }
         let event = swarm.select_next_some().await;
         // println!("{:?}", event);
@@ -177,22 +229,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
             SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {}", address),
             SwarmEvent::Behaviour(BehaviourEvent::Identify(x)) => match x {
                 identify::Event::Received { peer_id, info, .. } => {
-                    for addr in info.listen_addrs {
-                        swarm
-                            .behaviour_mut()
-                            .kademlia
-                            .add_address(&peer_id, addr.clone());
-                        if !swarm.is_connected(&peer_id) {
-                            if let Err(e) = swarm.dial(addr.clone()) {
-                                println!(
-                                    "Failed to dial peer {:?} at {:?}: {:?}",
-                                    peer_id, addr, e
-                                );
-                            }
-                        }
-                    }
+                    new_peer(&mut swarm, &peer_id, info);
                 }
-                _ => println!("{:?}", x),
+                _ => {}
             },
             SwarmEvent::Behaviour(BehaviourEvent::Kademlia(x)) => {
                 handle_kad(
@@ -202,6 +241,26 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     key.clone().into(),
                 )
                 .await;
+            }
+            SwarmEvent::ExternalAddrConfirmed { address } => {
+                println!(
+                    "Mode updated to {} at {}",
+                    swarm.behaviour_mut().kademlia.mode(),
+                    address
+                )
+            }
+            SwarmEvent::ExternalAddrExpired { address } => {
+                println!(
+                    "Could not confirm external address: {}, switching to server mode",
+                    address
+                );
+                swarm
+                    .behaviour_mut()
+                    .kademlia
+                    .set_mode(Some(kad::Mode::Server));
+            }
+            SwarmEvent::OutgoingConnectionError { error, .. } => {
+                println!("Error dialing: {}", error)
             }
             _ => {}
         }
